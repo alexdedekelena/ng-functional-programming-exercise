e00aa4c2d3f2708298156bc4e2252cc38d9dcb4e
alexander <alexdedekelena@gmail.com>
2025-06-02 15:42:09 +0200

    feat: installed PRIME NG for layout components - Add Home and Navbar components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { FunctionalTokeniserService } from './services/functional-tokeniser-serv
 import { MapReduceService } from './services/map-reduce-service.service';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./core/layout/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
